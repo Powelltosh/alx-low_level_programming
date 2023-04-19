f6e9a45ceea03b8002b41b8b835036830a8d67e5
Powelltosh <powellokinyo@gmail.com>
2023-04-19 11:49:45 -0700

    100-main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,97 +1,40 @@
-#include <stdlib.h>
 #include <stdio.h>
-
-/**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
- *
- * Return: void
- */
-void print_opcodes(char *a, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
-	}
-	printf("\n");
-
-}
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- *
- * Return: always O
- */
-int main(int argc, char **argv)
-{
-	int n;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	n = atoi(argv[1]);
-	if (n < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	print_opcodes((char *)&main, n);
-	return (0);
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
+ * main - entry point
  *
- * Return: void
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: always 0
  */
-void print_opcodes(char *a, int n)
+int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
-	}
-	printf("\n");
-
+int i, n;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
 }
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- *
- * Return: always O
- */
-int main(int argc, char **argv)
+n = atoi(argv[1]);
+if (n < 0)
 {
-	int n;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	n = atoi(argv[1]);
-	if (n < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	print_opcodes((char *)&main, n);
-	return (0);
+printf("Error\n");
+exit(2);
+}
+for (i = 0; i < n; i++)
+{
+printf("%02hhx", *((char *)main + i));
+if (i < n - 1)
+{
+printf(" ");
+}
+else
+{
+printf("\n");
+}
+}
+return (0);
 }
